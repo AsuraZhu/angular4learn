bf6f270d1de11d45b0fd74236f1e8ffd7e121a92
zmm <1614180462@qq.com>
2017-12-06 16:36:14 +0800

    学习和使用rxjs构建angular http

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -8,7 +8,8 @@ import { Hero } from '../hero';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: ['./hero-detail.component.css'],
+  providers: [HeroService]
 })
 export class HeroDetailComponent implements OnInit {
   hero: Hero;
