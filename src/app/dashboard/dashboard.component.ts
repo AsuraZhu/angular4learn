7273a87ef556832cbc1a5ed041c502283d873a23
zmm <1614180462@qq.com>
2017-12-08 08:36:09 +0800

    学习和使用表单

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,13 +4,19 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
 
   heroes: Hero[];
+  submitted = false;
+  // name =  'zs';
+  test = 'zs';
+  model = new Hero(18, 'Dr IQ', 'Really Smar', 'Chuck Overstreet');
   constructor(private heroService: HeroService) { }
-
+  onSubmit(): void {
+    this.submitted = true;
+  }
   ngOnInit(): void {
     this.heroService.getHeroes()
       .then(heroes => { console.log(heroes[2].name); this.heroes = heroes; } );
