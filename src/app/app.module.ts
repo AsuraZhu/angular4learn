563f2519a1419faf63629cd5b22eece1b32636a9
zmm <1614180462@qq.com>
2017-12-08 15:59:38 +0800

    学习使用表单

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ClickMeComponent } from './click-me.component';
 import { MyconpentComponent } from './myconpent/myconpent.component';
@@ -30,6 +30,7 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     FormsModule,
     AppRoutingModule,
     HttpModule,
+    ReactiveFormsModule
     // InMemoryWebApiModule.forRoot(InMemoryDataService),
   ],
   providers: [HeroService],
